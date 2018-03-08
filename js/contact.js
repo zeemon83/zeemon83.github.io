955edece6a5d2a99cf1b72ae3dea12332c54ba40
zeemon83 <zeemon777@gmail.com>
2018-03-08 07:23:39 -0800

    Add files via upload

--- js/contact.js
+++ js/contact.js
@@ -3,8 +3,22 @@ const img = document.getElementById('img');
 const address = document.getElementById('address');
 
 button.addEventListener('click', () => {
-  img.src = 'img/compute2.jpg';
-  address.innerHTML = 'zeemon777@gmail.com';
-  button.style.opacity = '0';
-  button.style.marginTop = '0';
+  if (address.innerHTML == '') {
+    img.src = 'img/compute2.jpg';
+    address.innerHTML = 'zeemon777@gmail.com';
+    button.style.marginTop = '0';
+
+  } else {
+    img.src = '';
+    address.innerHTML = '';
+    button.style.marginTop = '200px';
+
+  }
+
 });
+
+
+// img.src = 'img/compute2.jpg';
+// address.innerHTML = 'zeemon777@gmail.com';
+// button.style.opacity = '0';
+// button.style.marginTop = '0';
--- js/home.js
+++ js/home.js
@@ -1,17 +1,46 @@
 const cancelled = document.getElementsByClassName('cancelled');
 const white = document.getElementsByClassName('white-bk');
 const itemPad = document.getElementById('tour');
+const contain3 = document.querySelector('.container3');
+const itemDiv = document.querySelector('.item-pad');
 
-function noShow() {
-  for (var i = 0; i < white.length; i++) {
-    white[i].style.textDecoration = 'line-through';
-    white[i].style.textDecorationColor = 'red';
-  }
-  for (var i = 0; i < cancelled.length; i++) {
-    cancelled[i].innerHTML = 'cancelled';
-    cancelled[i].style.textTransform = 'uppercase';
+contain3.addEventListener('mouseover', (event) => {
+  if (event.target.tagName == 'LI') {
+    event.target.style.textDecoration = 'line-through';
+    event.target.style.textDecorationColor = 'red';
   }
 
-}
+});
 
-itemPad.addEventListener('mouseover', noShow);
+contain3.addEventListener('mouseover', (event) => {
+  if (event.target.tagName == 'SPAN') {
+    event.target.innerHTML = ' - cancelled';
+    event.target.style.textDecoration = 'line-through';
+    event.target.style.textDecorationColor = 'red';
+
+    event.target.style.textTransform = 'uppercase';
+  }
+
+});
+
+
+
+
+
+
+
+
+// function noShow() {
+//   for (var i = 0; i < white.length; i++) {
+//     white[i].style.textDecoration = 'line-through';
+//     white[i].style.textDecorationColor = 'red';
+//   }
+//
+//   for (var i = 0; i < cancelled.length; i++) {
+//     cancelled[i].innerHTML = 'cancelled';
+//     cancelled[i].style.textTransform = 'uppercase';
+//   }
+//
+// }
+//
+// itemPad.addEventListener('mouseover', noShow);
