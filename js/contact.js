5b2e37e7a2098dc8fa115bb1b2bac4bca6ff5d6d
zeemon83 <zeemon777@gmail.com>
2018-03-08 10:22:07 -0800

    Add files via upload

--- js/contact.js
+++ js/contact.js
@@ -6,12 +6,12 @@ button.addEventListener('click', () => {
   if (address.innerHTML == '') {
     img.src = 'img/compute2.jpg';
     address.innerHTML = 'zeemon777@gmail.com';
-    button.style.marginTop = '0';
+    // button.style.marginTop = '0';
 
   } else {
     img.src = '';
     address.innerHTML = '';
-    button.style.marginTop = '494px';
+    // button.style.marginTop = '494px';
 
   }
 
